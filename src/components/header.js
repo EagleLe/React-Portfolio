f6af173b3959ec2e60dc7722f2c19bf06354bbc9
eagle <eagle.lam.le@Gmail.com>
2021-08-22 17:17:25 -0400

    First page and basic information

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Hi , I am
+Eagle Le </h1>
+<div className="col-md-5 mt-4">
+ <p class="subheader subheader--2">A full stack web developer</p>
+
+		
+
+</div> 
+<div>
+<a href="/contact" className="btn btn-info">Contact Me</a>
+
+
+</div>
+
+            </div>
+            
+        </div>
+    )
+}
