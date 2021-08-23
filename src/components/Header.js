115343b0f3bac1e394d6a3fc513bebf1a0c77dd4
eagle <eagle.lam.le@Gmail.com>
2021-08-23 09:55:00 -0400

    profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import pic2 from './../eagle.jpg';
+
 
 export const Header = () => {
     return (
@@ -12,14 +14,17 @@ Eagle Le </h1>
 		
 
 </div> 
+
+<div className="col-md-5 mt-4">
+            <img className="img2" src={pic2} />
+            </div>
 <div>
 <a href="/contact" className="btn btn-info">Contact Me</a>
 
 
 </div>
 
-            </div>
-            
         </div>
-    )
+    </div>
+                )
 }
