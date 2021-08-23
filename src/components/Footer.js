c6d91ee8d7c9cb50e7960c0c16fc3e3446fdf017
eagle <eagle.lam.le@Gmail.com>
2021-08-22 21:09:23 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+      
+          <div className="bottom section-padding">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 text-center">
+						<div className="copyright">
+							<p>© <span>2021</span> <a href="#" className="transition">Eagle Le</a> All rights reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+         
+    )
+}
